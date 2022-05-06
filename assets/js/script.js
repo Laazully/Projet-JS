e759e42114e3b21dee6321e369f93e88c175120a
mariefnt <frenot.marie@free.fr>
2022-05-06 12:31:28 +0200

    fusion horrible

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var pseudo = document.querySelector('.input').value;
+var btnGo = document.querySelector('.btn-GO');
+var pseudoUser = document.querySelector('.userName');
+
+window.addEventListener('load', function() {
+    
+})
