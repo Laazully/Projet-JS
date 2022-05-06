0f8a73a9e72a23eafc082aa8129b05c939439eb5
Elisa.J <elisa.joudal@gmail.com>
2022-05-06 13:43:01 +0200

    commit accueil 2

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var pseudo = document.querySelector('.input');
+console.log(pseudo);
+var btnGo = document.querySelector('.btn-GO');
+var pseudoUser = document.querySelector('.userName');
+
+btnGo.addEventListener('click', function(){
+    return
+    // ecouter le bouton 
+    // recuperer la valeur de l'input
+
+    
+
+//     pseudoUser=pseudo.value
+//     console.log(pseudoUser);
+// })
