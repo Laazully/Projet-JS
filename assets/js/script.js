14ff5214fd768145cdb4eb57e191565cbca22ab0
mariefnt <frenot.marie@free.fr>
2022-05-09 09:12:10 +0200

    modification we

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,11 @@ var pseudo = document.querySelector('.userName');
 btnGo.addEventListener('click', function(){
     divHome.style.display = 'none';
     divBattle.style.display = 'block';
-    pseudo.textContent =getPseudo.value;
+    if (getPseudo.value === ''){
+        pseudo.textContent ='User';
+    } else {
+        pseudo.textContent =getPseudo.value;
+    }
     
 })
 
@@ -28,21 +32,19 @@ btnReturn.addEventListener('click', function(){
 var btnPierre = document.querySelector('.pierre');
 var btnFeuille = document.querySelector('.feuille');
 var btnCiseaux = document.querySelector('.ciseaux');
-var divTotoChoice = document.querySelector('.totoChoice');
-var divCompChoice = document.querySelector('.compChoice')
-var totoPierre = document.querySelector('.totoPierre');
-var totoFeuille = document.querySelector('.totoFeuille');
-var totoCiseaux = document.querySelector('.totoCiseaux');
-var ordiPierre = document.querySelector('.ordiPierre');
-var ordiFeuille = document.querySelector('.ordiFeuille');
-var ordiCiseaux = document.querySelector('.ordiCiseaux');
+
+var divComputerChoice = document.querySelector('.compChoice')
+var resultWin= document.querySelector('.win');
+var resultLoose = document.querySelector('.loose');
+var resultEquality = document.querySelector('.equality');
+var userScore = 0;
+var computerScore= 0;
+var getUserScore = document.querySelector('.scoreUser')
+var getComputerScore = document.querySelector('.scoreComp')
 
 btnPierre.addEventListener('click', function(){
-    // totoPierre.classList.add='show';
-    // if btnPierre('click')
-
     // Injecter l'image cliquée
-    var divPlayerChoice = document.querySelector('.totoChoice');
+    var divPlayerChoice = document.querySelector('.userChoice');
     divPlayerChoice.innerHTML = btnPierre.outerHTML;
 
     // Choisir de manière aléatoire le signe du computeur
@@ -53,37 +55,136 @@ btnPierre.addEventListener('click', function(){
 
     // Injecter l'image du signe du computeur
     // Déterminer le gagnant entre le signe player et le signe computer
-    var divComputerChoice = document.querySelector('.compChoice')
-    var resultWin= document.querySelector('.win');
-    var resultLoose = document.querySelector('.loose');
-    var resultEquality = document.querySelector('.equality');
+    // **Injecter en fonction du gagnant si c'est gagné, perdu ou égalité
+
     if(rValue === 'pierre') {
         divComputerChoice.innerHTML = '<img class="ordiPierre" src="/assets/img/pierre.png" alt="pierre">';
-        // Egalité
-        resultEquality.style.display="flex";
+        //** */ Egalité
+        
+        // resultEquality.style.display="flex";
+        // Score +0
+        
     } else if(rValue === 'ciseaux') {
         divComputerChoice.innerHTML = '<img class="ordiCiseaux" src="/assets/img/ciseaux.png" alt="ciseaux">';
-        // Gagné
-        resultWin.style.display="flex";
+        //** */ Gagné
+        // resultWin.style.display="flex";
+        // Score +1
+        userScore ++; // Incrémenter le score du gagnant (computeur ou player)
+        getUserScore.innerHTML = userScore; // Injecter le résultat
     } else { (rValue === 'feuille') 
         divComputerChoice.innerHTML ='<img class="ordiFeuille" src="/assets/img/feuille.png" alt="feuille"></img>';
-        // Perdu
-        resultLoose.style.display="flex";
+        //** */ Perdu
+        // resultLoose.style.display="flex";
+        // Score +0
+        computerScore++;
+        getComputerScore.innerHTML = computerScore;
     }
-    
-
-    // Injecter en fonction du gagnant si c'est gagné, perdu ou égalité
-    
-
-
-    // Incrémenter le score du gagnant (computeur ou player)
-
-
-
-    // Injecter le résultat
 })
 
 
+btnFeuille.addEventListener('click', function(){
+    // Injecter l'image cliquée
+    var divPlayerChoice = document.querySelector('.userChoice');
+    divPlayerChoice.innerHTML = btnFeuille.outerHTML;
 
-// console.log(rValue);
+    // Choisir de manière aléatoire le signe du computeur
+    var choices = document.querySelectorAll('.choices');
+    var myChoices = ['pierre','feuille','ciseaux'];
+    var rand = Math.random()*3 | 0;
+    var rValue = myChoices[rand];
+
+    // Injecter l'image du signe du computeur
+    // Déterminer le gagnant entre le signe player et le signe computer
+    // **Injecter en fonction du gagnant si c'est gagné, perdu ou égalité
+
+
+    if(rValue === 'pierre') {
+        divComputerChoice.innerHTML = '<img class="ordiPierre" src="/assets/img/pierre.png" alt="pierre">';
+        //** */ Gagné
+        // resultWin.style.display="flex";
+        // Score +1
+        userScore ++; // Incrémenter le score du gagnant (computeur ou player)
+        getUserScore.innerHTML = userScore; // Injecter le résultat
+    } else if(rValue === 'ciseaux') {
+        divComputerChoice.innerHTML = '<img class="ordiCiseaux" src="/assets/img/ciseaux.png" alt="ciseaux">';
+        //** */ Perdu
+        // resultLoose.style.display="flex";
+        // Score +0
+        computerScore++;
+        getComputerScore.innerHTML = computerScore;
+    } else { (rValue === 'feuille') 
+    divComputerChoice.innerHTML ='<img class="ordiFeuille" src="/assets/img/feuille.png" alt="feuille"></img>';
+    //** */ Egalité
+        // resultEquality.style.display="flex";
+        // Score +0
+        
+    }
+})
+
+btnCiseaux.addEventListener('click', function(){
+    // Injecter l'image cliquée
+    var divPlayerChoice = document.querySelector('.userChoice');
+    divPlayerChoice.innerHTML = btnCiseaux.outerHTML;
+
+    // Choisir de manière aléatoire le signe du computeur
+    var choices = document.querySelectorAll('.choices');
+    var myChoices = ['pierre','feuille','ciseaux'];
+    var rand = Math.random()*3 | 0;
+    var rValue = myChoices[rand];
+
+    // Injecter l'image du signe du computeur
+    // Déterminer le gagnant entre le signe player et le signe computer
+    // **Injecter en fonction du gagnant si c'est gagné, perdu ou égalité
+
+
+    if(rValue === 'pierre') {
+        divComputerChoice.innerHTML = '<img class="ordiPierre" src="/assets/img/pierre.png" alt="pierre">';
+        //** */ Perdu
+        // resultLoose.style.display="flex";
+        // Score +0
+        computerScore++;
+        getComputerScore.innerHTML = computerScore;
+    } else if(rValue === 'ciseaux') {
+        divComputerChoice.innerHTML = '<img class="ordiCiseaux" src="/assets/img/ciseaux.png" alt="ciseaux">';
+        //** */ Egalité
+            // resultEquality.style.display="flex";
+            // Score +0
+            
+    } else { (rValue === 'feuille') 
+    divComputerChoice.innerHTML ='<img class="ordiFeuille" src="/assets/img/feuille.png" alt="feuille"></img>';
+    //** */ Gagné
+    // resultWin.style.display="flex";
+    // Score +1
+    userScore ++; // Incrémenter le score du gagnant (computeur ou player)
+    getUserScore.innerHTML = userScore; // Injecter le résultat
+    }
+
+   
+})
+
+//fermeture de la pop-up 
+
+// calcule du taux de victoire :
+var pourcent = document.querySelector('.persent');
+
+function winRate (getUserScore, getUcomputerScore){
+    var getpourcent = Math.round(getUserScore / (getUserScore + getComputerScore) * 100) + '%';
+    console.log(getpourcent);
+}
+
+
+// Au bout de 3 victoires, le round est terminé, le n° round change et score remis à zéro
+var getRoundNumber = document.querySelector('.round');
+var roundNumber = 1;
+
+function round (){
+    if (userScore =3){
+        roundNumber++;
+        getRoundNumber.innerHTML=roundNumber;
+    }
+}
+
+
+
+// Au bout de 5 round, la partie est terminé et renvoie vers resultat partie
 
